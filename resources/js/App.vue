3cdb8020b61862944d3f95088ec27a16c65e495a
Zhukov Paul <79101535010@yandex.ru>
2022-05-25 13:14:32 +0300

    Popup modal form / new top line

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,17 +4,36 @@
 
 <template>
     <div>
-        <popup-form-component></popup-form-component>
+         
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <!--button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a-- class="navbar-brand" href="#">Navbar</a-->
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <router-link to="/" class="nav-link ">Home</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/news" class="nav-link" >News</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/dashboard" class="nav-link" >Dashboard</router-link>
+            </li>
+          </ul>
+           <popup-form-component class="d-flex" ></popup-form-component> 
+        </div>
+      </div>
+    </nav>
 
-        <div class="container">
-            <div class="nav-scroller py-1 mb-2">
-                <nav class="nav d-flex justify-content-start">
-                    <router-link to="/" class="p-2 link-secondary">Home</router-link>
-                    <router-link to="/about" class="p-2 link-secondary">About</router-link>
-                    <router-link to="/news" class="p-2 link-secondary">News</router-link>
-                    <router-link to="/dashboard" class="p-2 link-secondary">Dashboard</router-link>
-                </nav> 
-            </div>
+         
+             
         </div>
 
     <main class="container">
